29ac4d9765416f4dac50aef6b8dc0a50eceac35f
omargad2211 <omargad7324@gmail.com>
2024-12-03 17:56:43 +0200

    fix:fixing card sizes

--- src/pages/Home/Components/ why Choose Sophomore/ChooseCard.jsx
+++ src/pages/Home/Components/ why Choose Sophomore/ChooseCard.jsx
@@ -32,26 +32,26 @@ export default function ChooseCard() {
           </span>
         </p>
       </div>
-      <div className="flex justify-center md:justify-between items-center flex-wrap gap-4 pt-6 ">
+      <div className="flex justify-center md:justify-between items-center flex-wrap  md:flex-row gap-4 pt-6 ">
         {Sophomore.map((item) => {
           return (
             <>
               <div
                 key={item.id}
-                className="max-w-72 min-h-[252px] rounded-[5px] bg-white cursor-pointer flex flex-col items-center  justify-center gap-y-5 p-5"
+                className="w-80 md:min-w-96 min-h-[292px] md:min-h-[350px]  xl: rounded-[5px] bg-white cursor-pointer flex  flex-col items-center  justify-center gap-y-5 p-5"
               >
                 <div>
                   <img
                     src={item.img}
                     alt={item.Name}
-                    className="w-20 h-20 object-cover rounded-md"
+                    className="size-20 xl:size-32  object-cover rounded-md"
                   />
                 </div>
 
-                <p className="text-base font-medium leading-[18.75px] text-center text-black ">
+                <p className="text-base xl:text-xl  font-medium leading-[18.75px] text-center text-black ">
                   {item.Name}
                 </p>
-                <p className="text-sm font-normal leading-[16.41px] text-center text-[#0000007D] ">
+                <p className="text-sm xl:text-lg font-normal leading-[16.41px] text-center text-[#0000007D] ">
                   {item.dec}
                 </p>
               </div>
