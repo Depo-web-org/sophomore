462c4e9cc1c906127ec111a043d2f466ef4c0708
omargad2211 <omargad7324@gmail.com>
2024-12-03 16:54:56 +0200

    fix:hashing grades nested rout till json server

--- src/pages/Grades/index.jsx
+++ src/pages/Grades/index.jsx
@@ -1,27 +1,28 @@
-import { staticSchools } from "../../Helpers/Static";
-import { Link, useParams, Outlet } from "react-router-dom";
+// import { staticSchools } from "../../Helpers/Static";
+// import { Link, useParams, Outlet } from "react-router-dom";
 
 export const Grade = () => {
-  const { schoolId } = useParams();
-  console.log(schoolId);
-  const school = staticSchools.find((s) => s.id === schoolId);
-  console.log(school);
+  //   const { schoolId } = useParams();
+  //   console.log(schoolId);
+  //   const school = staticSchools.find((s) => s.id === schoolId);
+  //   console.log(school);
   return (
-    <div>
-      <h2>{school.name} - Grades</h2>
-      <ul>
-        {school.grades.map((grade) => (
-          <li key={grade.id}>
-            <Link
-              to={`/school/${schoolId}/grade/${grade.id}`}
-              className="bg-red-400"
-            >
-              {grade.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <Outlet />
-    </div>
+    <></>
+    // <div>
+    //   <h2>{school.name} - Grades</h2>
+    //   <ul>
+    //     {school.grades.map((grade) => (
+    //       <li key={grade.id}>
+    //         <Link
+    //           to={`/school/${schoolId}/grade/${grade.id}`}
+    //           className="bg-red-400"
+    //         >
+    //           {grade.name}
+    //         </Link>
+    //       </li>
+    //     ))}
+    //   </ul>
+    //   <Outlet />
+    // </div>
   );
 };
