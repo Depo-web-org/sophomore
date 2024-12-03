468b7dd63cbc2843f96842ca079f79f44bc61282
omargad2211 <omargad7324@gmail.com>
2024-12-03 17:43:10 +0200

    fix:fix choose card in home page

--- src/pages/Home/Components/ why Choose Sophomore/ChooseCard.jsx
+++ src/pages/Home/Components/ why Choose Sophomore/ChooseCard.jsx
@@ -6,48 +6,56 @@ export default function ChooseCard() {
       dec: "Customized curriculum based on your goals and current skill level",
       img: "/public/Schools/Group-1.svg",
     },
-    // {
-    //   id: "Expert",
-    //   Name: "Expert Instructors",
-    //   img: "/public/Schools/Icon-2.svg",
+    {
+      id: "Expert",
+      Name: "Expert Instructors",
+      dec: "Learn from industry professionals with real-world experience",
 
-    // },
-    // {
-    //   id: "Interactive",
-    //   Name: "Interactive Content",
-    //   img: "/public/Schools/Icon-3.svg",
+      img: "/public/Schools/Icon-2.svg",
+    },
+    {
+      id: "Interactive",
+      Name: "Interactive Content",
+      dec: "Engage with hands-on projects and real-time feedback",
 
-    // },
+      img: "/public/Schools/Icon-3.svg",
+    },
   ];
 
   return (
-    <section className="min-h-screen">
+    <section className="min-h-[498px] py-7">
       <div className="text-white pb-4">
-        <span className="block text-2xl lg:text-4xl font-bold leading-10">
-          Why Choose Sophomore
-        </span>
+        <p className=" text-2xl lg:text-4xl font-bold leading-10 ">
+          Why Choose{" "}
+          <span className="bg-gradient-to-r from-primary  via-primary uppercase   to-secondary bg-clip-text text-transparent inline-block">
+            Sophomore
+          </span>
+        </p>
       </div>
-
-      <div className="w-96 bg-white cursor-pointer flex flex-col items-center justify-center">
+      <div className="flex justify-center md:justify-between items-center flex-wrap gap-4 pt-6 ">
         {Sophomore.map((item) => {
           return (
-            <div
-              key={item.id}
-              className="flex flex-col items-center justify-center"
-            >
-              <div className="w-full h-36 flex justify-center items-center bg-red-300">
-                <img
-                  src={item.img}
-                  alt={item.Name}
-                  className="w-20 h-20 object-cover rounded-md"
-                />
-              </div>
+            <>
+              <div
+                key={item.id}
+                className="max-w-72 min-h-[252px] rounded-[5px] bg-white cursor-pointer flex flex-col items-center  justify-center gap-y-5 p-5"
+              >
+                <div>
+                  <img
+                    src={item.img}
+                    alt={item.Name}
+                    className="w-20 h-20 object-cover rounded-md"
+                  />
+                </div>
 
-              <div className="bg-blue-400  text-black font- normal text-center w-full pb-10">
-                  <span className="font-bold text-md block ">{item.Name}</span>
-                  <span className="text-md font-medium ">{item.dec}</span>
+                <p className="text-base font-medium leading-[18.75px] text-center text-black ">
+                  {item.Name}
+                </p>
+                <p className="text-sm font-normal leading-[16.41px] text-center text-[#0000007D] ">
+                  {item.dec}
+                </p>
               </div>
-            </div>
+            </>
           );
         })}
       </div>
