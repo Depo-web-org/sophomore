57086d90ad240af0a80836e47184955bb16896e5
omargad2211 <omargad7324@gmail.com>
2024-12-04 10:02:06 +0200

    feat:add grades page layout

--- src/pages/Grades/index.jsx
+++ src/pages/Grades/index.jsx
@@ -5,7 +5,7 @@ import CardInfo from "../../Components/Common/CardInfo/CardInfo";
 
 const schoolGrades = [
   {
-    name: "Elementary",
+    Name: "Elementary",
     grades: [
       {
         Name: "1 grade",
@@ -34,27 +34,43 @@ const schoolGrades = [
     ],
   },
   {
-    Name: "Middle School",
+    Name: "Preparatory/Middle School",
     grades: [
-      { Name: "7 grade", img: "image_url_for_grade_7.jpg" },
-      { Name: "8 grade", img: "image_url_for_grade_8.jpg" },
-      { Name: "9 grade", img: "image_url_for_grade_9.jpg" },
+      {
+        Name: "7 grade",
+        img: "https://s3-alpha-sig.figma.com/img/9c8a/52d9/40dc459658c7a9cce0362fa37ed69042?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=JeVx47-Tbm6xutpo2CbyNlhCdUb2ML3YMhRHwARuK8qDn-OYnAAO~27R88PMOaOnCeJkUf-SHf-RS10xaPGa5kFzqxs6L6EKU1j09udsUMcErHyuoCCTnvxmXDnTGutHfwRnwyrr7uDzhSAIYM6it5ewEdj33SZ7p6oik0ttdlKl-ViimIpI3H8dOjwWDvDmp9woPSWXg9vGcEARLscsixaGYLsLm0OSPHHytfDMBy9ZCpzTrc2Vs62wDKMnFuRmLFPBsqx6yV0y9F0w-sg3TLjENGgNUg3HTVm~ycXFVdoQhsdz3El1GZHG8T4m2wfmNjkXcAMVp2ALxIRJhV183Q__",
+      },
+      {
+        Name: "8 grade",
+        img: "https://s3-alpha-sig.figma.com/img/9c8a/52d9/40dc459658c7a9cce0362fa37ed69042?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=JeVx47-Tbm6xutpo2CbyNlhCdUb2ML3YMhRHwARuK8qDn-OYnAAO~27R88PMOaOnCeJkUf-SHf-RS10xaPGa5kFzqxs6L6EKU1j09udsUMcErHyuoCCTnvxmXDnTGutHfwRnwyrr7uDzhSAIYM6it5ewEdj33SZ7p6oik0ttdlKl-ViimIpI3H8dOjwWDvDmp9woPSWXg9vGcEARLscsixaGYLsLm0OSPHHytfDMBy9ZCpzTrc2Vs62wDKMnFuRmLFPBsqx6yV0y9F0w-sg3TLjENGgNUg3HTVm~ycXFVdoQhsdz3El1GZHG8T4m2wfmNjkXcAMVp2ALxIRJhV183Q__",
+      },
+      {
+        Name: "9 grade",
+        img: "https://s3-alpha-sig.figma.com/img/9c8a/52d9/40dc459658c7a9cce0362fa37ed69042?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=JeVx47-Tbm6xutpo2CbyNlhCdUb2ML3YMhRHwARuK8qDn-OYnAAO~27R88PMOaOnCeJkUf-SHf-RS10xaPGa5kFzqxs6L6EKU1j09udsUMcErHyuoCCTnvxmXDnTGutHfwRnwyrr7uDzhSAIYM6it5ewEdj33SZ7p6oik0ttdlKl-ViimIpI3H8dOjwWDvDmp9woPSWXg9vGcEARLscsixaGYLsLm0OSPHHytfDMBy9ZCpzTrc2Vs62wDKMnFuRmLFPBsqx6yV0y9F0w-sg3TLjENGgNUg3HTVm~ycXFVdoQhsdz3El1GZHG8T4m2wfmNjkXcAMVp2ALxIRJhV183Q__",
+      },
     ],
   },
   {
-    Name: "High School",
+    Name: "Secondary/High School",
     grades: [
-      { Name: "10 grade", img: "image_url_for_grade_10.jpg" },
-      { Name: "11 grade", img: "image_url_for_grade_11.jpg" },
-      { Name: "12 grade", img: "image_url_for_grade_12.jpg" },
+      {
+        Name: "10 grade",
+        img: "https://s3-alpha-sig.figma.com/img/cb58/45ee/4d2f30c9f5dee4515e0c9e6c2e391157?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pCCuPy3sxA32WVSvf9DIcwSsAF3e3~5oSIDi0aY8y0Wl~FxQtuHMa5Fs4qn2Vwb4Iu68pjz0O2SdN~d-RKfj4uviFkHOJQVWq~0J7C1u8AztCT8QDpyTOEXZSQmdYKspExhqOn1VBydHKkE6lE4cDjxCqpN6w-mzhaT7k0PbVBZTuq54SuanCOzbQJNTxRPbzmj43rxAsRXQrfN~rh9lz6W3EluXiKm9iTDyuMHojPNhHak75dOrF0SFBGRyBazn9dSDM~QGqolJpkIxlgaWgQm0WbP9wFjqI4wpUdEhE3JKUNJ33OOqdLfJ3zTEnIDRjIsnYOv-gcjc5Cb6MesCiw__",
+      },
+      {
+        Name: "11 grade",
+        img: "https://s3-alpha-sig.figma.com/img/cb58/45ee/4d2f30c9f5dee4515e0c9e6c2e391157?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pCCuPy3sxA32WVSvf9DIcwSsAF3e3~5oSIDi0aY8y0Wl~FxQtuHMa5Fs4qn2Vwb4Iu68pjz0O2SdN~d-RKfj4uviFkHOJQVWq~0J7C1u8AztCT8QDpyTOEXZSQmdYKspExhqOn1VBydHKkE6lE4cDjxCqpN6w-mzhaT7k0PbVBZTuq54SuanCOzbQJNTxRPbzmj43rxAsRXQrfN~rh9lz6W3EluXiKm9iTDyuMHojPNhHak75dOrF0SFBGRyBazn9dSDM~QGqolJpkIxlgaWgQm0WbP9wFjqI4wpUdEhE3JKUNJ33OOqdLfJ3zTEnIDRjIsnYOv-gcjc5Cb6MesCiw__",
+      },
+      {
+        Name: "12 grade",
+        img: "https://s3-alpha-sig.figma.com/img/cb58/45ee/4d2f30c9f5dee4515e0c9e6c2e391157?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pCCuPy3sxA32WVSvf9DIcwSsAF3e3~5oSIDi0aY8y0Wl~FxQtuHMa5Fs4qn2Vwb4Iu68pjz0O2SdN~d-RKfj4uviFkHOJQVWq~0J7C1u8AztCT8QDpyTOEXZSQmdYKspExhqOn1VBydHKkE6lE4cDjxCqpN6w-mzhaT7k0PbVBZTuq54SuanCOzbQJNTxRPbzmj43rxAsRXQrfN~rh9lz6W3EluXiKm9iTDyuMHojPNhHak75dOrF0SFBGRyBazn9dSDM~QGqolJpkIxlgaWgQm0WbP9wFjqI4wpUdEhE3JKUNJ33OOqdLfJ3zTEnIDRjIsnYOv-gcjc5Cb6MesCiw__",
+      },
     ],
   },
 ];
 
 export const Grade = () => {
   const { schoolName } = useParams();
-  console.log(schoolName);
-  console.log(schoolGrades[0].Name, schoolGrades[0].grades[0].Name);
   return (
     <>
       <div className="bg-slate-900 min-h-screen pt-24 px-4 lg:px-[124px]">
@@ -63,17 +79,20 @@ export const Grade = () => {
             What is your current school grade?
           </p>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col py-3">
           {schoolGrades.map((grade) => (
-            <div key={grade.Name}>
-              <p className="text-white text-3xl">{grade.Name}</p>
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+            <div className="pt-8" key={grade.Name}>
+              <p className="text-white text-2xl font-semibold leading-[42px]">
+                {grade.Name}
+              </p>
+              <div className="grid grid-cols-6  lg:grid-cols-12 gap-4 ">
                 {grade.grades.map((gradeItem) => (
-                  <CardInfo
-                    key={gradeItem.img}
-                    item={gradeItem}
-                    path={`/school/${schoolName}/grade/${gradeItem.Name}`}
-                  />
+                  <div key={gradeItem.img} className="col-span-3 lg:col-span-4">
+                    <CardInfo
+                      item={gradeItem}
+                      path={`/school/${schoolName}/grade/${gradeItem.Name}`}
+                    />
+                  </div>
                 ))}
               </div>
             </div>
