6268c2fb31eb885c4657d8ebb8a9f81154fc1d0f
omargad2211 <omargad7324@gmail.com>
2024-12-04 10:58:34 +0200

    add:adding teachers layout

--- src/pages/Teachers/Teachers.jsx
+++ src/pages/Teachers/Teachers.jsx
@@ -1,17 +1,9 @@
-
 const Teachers = () => {
   return (
     <>
-    <section className="min-h-screen bg-emerald-700 w-full">
-
-    </section>
-    
-    
-    
-    
-    
+      <section className="min-h-screen w-full pt-24 px-4 lg:px-[124px]"></section>
     </>
-  )
-}
+  );
+};
 
-export default Teachers
+export default Teachers;
