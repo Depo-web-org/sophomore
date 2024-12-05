246c378e81e4ed50044123fbd5a3df851c9f1114
omargad2211 <omargad7324@gmail.com>
2024-12-05 10:58:40 +0200

    feat:crate border animation to the nav bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,45 +1,50 @@
-import logo from '../../../public/logos/logo.svg'
-import { Link, NavLink } from "react-router-dom"
-import './style.css'
-import { useMemo, useState } from 'react';
-import { HiMenu, HiX } from 'react-icons/hi';
-import { IoCartOutline, IoHeartOutline } from 'react-icons/io5';
+import logo from "../../../public/logos/logo.svg";
+import { Link, NavLink } from "react-router-dom";
+import "./style.css";
+import { useMemo, useState } from "react";
+import { HiMenu, HiX } from "react-icons/hi";
+import { IoCartOutline, IoHeartOutline } from "react-icons/io5";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const navItems = useMemo(() => ([
-    { text: `Home`, link: "/" },
-    { text: `About Us`, link: "/about" },
-    { text: `Contact Us`, link: "/contact" },
-    { icon: <IoCartOutline />, link: "/cart" },
-    { icon: <IoHeartOutline />, link: "/" },
-  ]), []);
+  const navItems = useMemo(
+    () => [
+      { text: `Home`, link: "/" },
+      { text: `About Us`, link: "/about" },
+      { text: `Contact Us`, link: "/contact" },
+      { icon: <IoCartOutline />, link: "/cart" },
+      { icon: <IoHeartOutline />, link: "/" },
+    ],
+    []
+  );
 
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
   return (
     <nav className="z-50 fixed top-4 w-full px-4 md:px-0">
       <div className="bg-white/20 shadow-black/10 backdrop-blur-[5px] border border-white/20 w-full md:w-[calc(100%-124px)] xl:w-4/5 transition-all duration-300 h-16 mx-auto shadow-sm rounded-full flex items-center justify-between">
-        
         <Link to={"/"}>
           <div className="flex justify-start items-center pt-1 ml-5">
-            <img src={logo} alt="logo" className="h-8 w-auto"/>  
+            <img src={logo} alt="logo" className="h-8 w-auto" />
             <p className="bg-gradient-to-r from-primary via-primary to-secondary bg-clip-text text-transparent text-xl md:text-2xl font-bold ml-2">
               Sophomore
             </p>
           </div>
-        </Link>  
+        </Link>
 
         <div className="hidden md:flex items-center mr-5">
           <div className="w-auto text-white flex items-center gap-8">
             <ul className="flex gap-x-7 font-semibold">
               {navItems.map((item, index) => (
-                <li key={index+item.text+item.link}>
-                   <NavLink to={item.link} className="text-white ">
-                   <span className='text-xl relative top-1 left-7'>{item.icon}</span>
-                   {item.text}
-                   </NavLink>
+                <li key={index + item.text + item.link}>
+                  <NavLink
+                    to={item.link}
+                    className="text-white text-base flex items-center justify-center "
+                  >
+                    <span className="text-xl"> {item.icon}</span>
+                    {item.text}
+                  </NavLink>
                 </li>
               ))}
             </ul>
@@ -58,8 +63,12 @@ const Navbar = () => {
         <div className="md:hidden absolute top-16 left-1/2 -translate-x-1/2 right-0 bg-white backdrop-blur-[5px] border border-white/20 shadow-md rounded-lg mt-2 py-4 px-6 w-5/6  ">
           <ul className="flex flex-col gap-y-4 font-semibold">
             {navItems.map((item, index) => (
-              <li key={index+item.text+item.link}>
-                <NavLink to={item.link} className="text-primary block" onClick={toggleMenu}>
+              <li key={index + item.text + item.link}>
+                <NavLink
+                  to={item.link}
+                  className="text-primary block"
+                  onClick={toggleMenu}
+                >
                   {item.text}
                 </NavLink>
               </li>
@@ -71,8 +80,7 @@ const Navbar = () => {
         </div>
       )}
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
-
+export default Navbar;
