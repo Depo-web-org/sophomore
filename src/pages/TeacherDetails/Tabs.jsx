94afc3c976f7e6f342d12463cb0d84a51cd29b06
omargad2211 <omargad7324@gmail.com>
2024-12-05 11:05:30 +0200

    fix:fix active border

--- src/pages/TeacherDetails/Tabs.jsx
+++ src/pages/TeacherDetails/Tabs.jsx
@@ -4,27 +4,27 @@ export default function Tabs() {
   const { schoolName, gradeName, subjectName, teacherName } = useParams();
   return (
     <div>
-      <div className="hidden sm:block">
-        <div className="border-b border-gray-200">
-          <nav className="-mb-px flex gap-6" aria-label="Tabs">
+      <div className="">
+        <div className="border-b  border-gray-200">
+          <nav className="mb-[4px] flex gap-6" aria-label="Tabs">
             <NavLink
               to={`/school/${schoolName}/grade/${gradeName}/subject/${subjectName}/teacher/${teacherName}`}
               end
-              className="shrink-0 border-b-2 border-transparent px-1 pb-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+              className="shrink-0 border-b-2 border-transparent px-1 pb-4 text-sm font-medium text-gray-500 hover:text-gray-700"
             >
               About
             </NavLink>
 
             <NavLink
               to="course-details"
-              className="shrink-0 border-b-2 border-transparent px-1 pb-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+              className="shrink-0 border-b-2 border-transparent px-1 pb-4 text-sm font-medium text-gray-500 hover:text-gray-700"
             >
               Course Details
             </NavLink>
 
             <NavLink
               to="reviews"
-              className="shrink-0 border-b-2 border-transparent px-1 pb-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+              className="shrink-0 border-b-2 border-transparent px-1 pb-4 text-sm font-medium text-gray-500 hover:text-gray-700"
             >
               Reviews
             </NavLink>
