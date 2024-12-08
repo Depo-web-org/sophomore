7ba3a7a22b75de70465d8935edce7f3594e3db9a
fawzybas <fawzy.basuony@depowebeg.com>
2024-12-08 10:01:03 +0200

    res home min

--- src/pages/Home/Components/Home min/HomeMin.jsx
+++ src/pages/Home/Components/Home min/HomeMin.jsx
@@ -22,33 +22,33 @@ export default function HomeMin() {
 
   return (
     <>
-      <section className=" text-white my-5 px-6">
+      <section className=" text-white my-10 px-6 ">
         <div className="py-5">
           <div className="flex justify-between">
             <span className="font-bold text-lg md:text-4xl">
               Let&apos;s Continue learning
             </span>
-            <p className="text-sm font-medium border-b-2 border-b-slate-700 pt-3">
+            <p className="text-sm font-medium border-b-2 border-b-slate-700 pt-3 hover:cursor-pointer">
               My learning
             </p>
           </div>
           <p className="text-sm font-medium ">Pick up from where you left</p>
         </div>
 
-        <div className="grid grid-cols-6 lg:grid-cols-12 gap-6 items-center justify-center">
+        <div className="grid grid-cols-6 lg:grid-cols-12 gap-6 items-center justify-center ">
           {HomeMin.map((item) => (
             <div
-              className="col-span-6 md:col-span-3 lg:col-span-4"
+              className="col-span-6 md:col-span-3 lg:col-span-4 overflow-hidden rounded-3xl"
               key={item.id}
             >
-              <div className="group relative block overflow-hidden rounded-lg">
+              <div className="group relative block ">
                 <img
                   src={item.img}
                   alt={item.Name}
-                  className="h-72 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
+                  className="w-full h-44 sm:h-48 lg:h-60 object-cover transition duration-500 group-hover:scale-105 overflow-hidden rounded-b-3xl"
                 />
 
-                <div className="font-semibold text-white mt-2 ">
+                <div className="font-semibold text-white mt-3 ps-2">
                   <p className="text-lg">{item.Name}</p>
 
                   <h3 className="text-lg">{item.dec}</h3>
