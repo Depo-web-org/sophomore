18e9acaf5385c6307c7e1cde6bbc9fd7d0f3c608
fawzybas <fawzy.basuony@depowebeg.com>
2024-12-08 17:12:53 +0200

    res home card

--- src/pages/Home/Components/choose/ChooseCard.jsx
+++ src/pages/Home/Components/choose/ChooseCard.jsx
@@ -34,30 +34,28 @@ const ChooseCard = () => {
       </div>
       <div className="flex justify-center md:justify-between items-center flex-wrap  md:flex-row gap-4 pt-6 ">
         {Sophomore.map((item) => {
-          return (  <div
-                key={item.id}
-                className=" w-full flex-initial sm:flex-auto md:flex-1 rounded-[5px] bg-white cursor-pointer flex  flex-col items-center  justify-center  lg:gap-y-5 py-6 px-4 xl:py-14 xl:px-8"
-              >
-                <div>
-                  <img
-                    src={item.img}
-                    alt={item.Name}
-                    className="size-16 md:size-20 xl:size-32  rounded-md"
-                  />
-                </div>
-
-                <p className="text-sm sm:text-base xl:text-xl  font-medium leading-[18.75px] text-center text-black ">
-                  {item.Name}
-                </p>
-                <p className="text-[12px] sm:text-sm xl:text-lg font-normal leading-[16.41px] text-center text-[#0000007D] ">
-                  {item.dec}
-                </p>
+          return (
+            <div
+              key={item.id}
+              className="sm:h-60 xl:h-auto w-full flex-initial sm:flex-auto md:flex-1 rounded-[5px] bg-white cursor-pointer flex  flex-col items-center  justify-center  lg:gap-y-5 py-6 px-4 xl:py-14 xl:px-8"
+            >
+              <div>
+                <img
+                  src={item.img}
+                  alt={item.Name}
+                  className="size-16 md:size-20 xl:size-32  rounded-md"
+                />
               </div>
+              <div className="text-sm sm:text-sm xl:text-lg font-normal text-center">
+                <p className="leading-[18.75px]  py-1 text-black">{item.Name}</p>
+                <p className="leading-[16.41px]  pt-1 text-[#0000007D]">{item.dec}</p>
+              </div>
+            </div>
           );
         })}
       </div>
     </section>
   );
-}
+};
 
-export default ChooseCard
+export default ChooseCard;
