24cdd298ed8b8b3aa85d18d48f5a9883e4c76647
os1907 <osamatrezey@gmail.com>
2024-12-08 17:48:05 +0200

    learningHero

--- src/pages/MyLearning/components/LearningHero.jsx
+++ src/pages/MyLearning/components/LearningHero.jsx
@@ -1,14 +1,74 @@
+import style from '../components/LearningHero.module.css'
 export default function LearningHero() {
   return (
     <section
-      className="text-white relative min-h-[606px] px-2 lg:px-[124px]"
-      style={{
-        backgroundImage:
-          "url(https://s3-alpha-sig.figma.com/img/abd9/13ef/b2c1333905ef55c42346b4bfec612a6e?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=m2B9bXzjF2vIDSbj8WV~AqezRthtYr7TgYEky6dn-drjRCzaXj~fMdQaulv2Z10jhrVYNKb~Purhyy3Dxhmvp9Mv5TSfYLVSczKUW8JqTEjv8o-~OAq4bQ-pKBHvFuyHwc451~in9lV98DcuzQLxH7GB-NlNWGlSnLTQmxwvw6RkuaRxw6yNWw1JmKxplDRf6dAMHoogow4maK4tgpTCcB1qorf5Jd~xIniwyaO~VFp8x7eJ-mXnT~FwL4nJKABh6JYGoNorYH2GBjzenU4pUWlu1d33pj-tAsVkymlWOQeaY1UVFuMsB6KHmsihbdSLShbURa0y0zSf2IZtPC6inw__)",
-        backgroundPosition: "center top",
-        backgroundSize: "cover",
-        objectFit: "cover",
-      }}
-    ></section>
+      className={`text-white relative min-h-[720px] bg  ${style.heroBg} flex flex-col items-center justify-center`}
+  
+    > 
+
+
+<div className="   w-full md:w-custom-md xl:w-custom-xl mx-auto py-32 grid grid-cols-12   relative z-10   ">
+  {/* Right  */}
+    <div className="col-span-12 lg:col-span-7 flex items-center  ">
+    <div className="  text-center   ">
+      <h2
+        className="text-white text-3xl lg:text-4xl xl:text-6xl font-extrabold text-transparent slide-in-top my-4 text-start "
+      >
+           Continue your learning Journey
+
+      </h2>
+
+      <p className=" sm:text-xl/relaxed font-normal slide-in-top-slow pb-2 lg:pb-0  text-start leading-7">
+      Access all the courses you’ve purchased and continue learning at your own pace.
+      </p>
+
+
+
+
+    </div>
+    </div>
+
+
+
+  {/* Left  */}
+
+    <div className="col-span-12 lg:col-span-5 flex justify-end  ">
+      <div className='bg-dark w-52 h-96 text-center p-8 rounded-3xl flex flex-col justify-between '>
+       <div>
+       <p className='font-bold text-xl ' >
+          Available Until
+        </p>
+        <div className='bg-zinc-300 rounded-3xl  overflow-hidden mt-5  m-9'>
+        <div className='bg-red-500 p-1   '>
+        <p className='font-bold text-xl ' >
+         Nov
+        </p>
+        </div>
+        <p className='text-zinc-800 text-3xl font-extrabold py-2'>
+          29  
+        </p>
+
+
+        </div>
+        <p className='text-white text-2xl font-semibold '>
+        2025
+        </p>
+       </div>
+
+
+
+       <div>
+        <p className='text-white font-semibold'>
+          328 days left
+        </p>
+       </div>
+
+      </div>
+    </div>
+
+    
+  </div>
+   <div className="bg-[#0A142FA6] h-full w-full absolute z-0 inset-0"></div>
+    </section>
   );
 }
