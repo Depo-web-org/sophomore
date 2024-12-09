4705bee1dbe1ed365fdc314111984b0e1ddcba81
omargad2211 <omargad7324@gmail.com>
2024-12-09 10:53:29 +0200

    feat:create course video layout

--- src/pages/CourseVideo/index.jsx
+++ src/pages/CourseVideo/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import CourseInfos from "./components/CourseInfos/CourseInfos";
+import Video from "./components/CourseVideo/CourseVideo";
 
 export default function CourseVideo() {
   return (
     <div className="min-h-screen w-full pt-24 container md:w-custom-md xl:w-custom-xl mx-auto 3">
-      <CourseInfos />
+      <div className="flex flex-col items-start justify-start gap-8 ">
+        <Video />
+        <CourseInfos />
+      </div>
     </div>
   );
 }
