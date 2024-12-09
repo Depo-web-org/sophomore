48727d2a81025aa9466a8e043e590a4c5f0e8afc
omargad2211 <omargad7324@gmail.com>
2024-12-09 11:59:41 +0200

    style:fix outlet sizes in profile

--- src/pages/Profile/components/Security/Security.jsx
+++ src/pages/Profile/components/Security/Security.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Security() {
-  return <div>Security</div>;
+  return (
+    <div className="w-full">
+      <div className="min-w-full bg-emerald-700">fdff</div>
+    </div>
+  );
 }
