560e5eebc0a8ae079cba44bb566f200c4ca76058
fawzybas <fawzy.basuony@depowebeg.com>
2024-12-15 11:18:14 +0200

    link bottom opation

--- src/pages/Teacher panel /pages/Courses/components/AddMessage.jsx
+++ src/pages/Teacher panel /pages/Courses/components/AddMessage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const AddMessage = () => {
   return (
@@ -34,14 +35,15 @@ const AddMessage = () => {
           placeholder="Enter any additional order notes..."
         ></textarea>
  
-         <button
+         <Link
+         to="/chooseunit"
           type="button"
           data-twe-ripple-init
           data-twe-ripple-color="light"
           className="w-full mt-5 rounded bg-primary px-2 py-2 text-md font-semibold text-white hover:bg-blue-800 transition-all duration-300"
         >
           Continue
-        </button>
+        </Link>
       </div>
     </>
   );
