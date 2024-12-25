75d1452c4f7b8f5e6cbd7f9b3cc2f51728ae2b8e
omargad2211 <omargad7324@gmail.com>
2024-12-25 14:28:53 +0200

    hash:hashing prepare header

--- src/Redux/api/apiSclice.js
+++ src/Redux/api/apiSclice.js
@@ -5,15 +5,15 @@ import { logOut, setCredentials } from "../Auth/authSlice";
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://192.168.1.26:8000/api/v1",
   credentials: "include",
-  prepareHeaders: (headers, { getState, endpoint }) => {
-    const token = getState().auth.token;
+  // prepareHeaders: (headers, { getState, endpoint }) => {
+  //   const token = getState().auth.token;
 
-    if (token && endpoint !== "login/consumer") {
-      headers.set("authorization", `Bearer ${token}`);
-    }
+  //   if (token && endpoint !== "login/consumer") {
+  //     headers.set("authorization", `Bearer ${token}`);
+  //   }
 
-    return headers;
-  },
+  //   return headers;
+  // },
 });
 
 // token refresh
@@ -47,5 +47,6 @@ const baseQueryWithReauth = async (args, api, extraOptions) => {
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: baseQueryWithReauth,
+  tagTypes: ["Auth"],
   endpoints: (builder) => ({}),
 });
