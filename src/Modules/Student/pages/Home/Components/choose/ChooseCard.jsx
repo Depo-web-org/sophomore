8fa85a9a51288f9444ae8b7ac2d5a34ab42b526c
os1907 <osamatrezey@gmail.com>
2025-01-14 12:05:31 +0200

    feat:treansaltion

--- src/Modules/Student/pages/Home/Components/choose/ChooseCard.jsx
+++ src/Modules/Student/pages/Home/Components/choose/ChooseCard.jsx
@@ -63,3 +63,56 @@ const ChooseCard = () => {
 };
 
 export default ChooseCard;
+
+
+
+
+
+
+
+// import { useTranslation } from "react-i18next";
+
+// const ChooseCard = () => {
+//   const { t } = useTranslation();
+//   const features = t("chooseCard.features", { returnObjects: true });
+
+//   return (
+//     <section className="min-h-[498px] pb-8 lg:pb-20">
+//       <div className="text-white pb-4 lg:pb-8">
+//         <p className="text-2xl lg:text-4xl font-bold md:leading-10 inline text-nowrap">
+//           {t("chooseCard.title")}
+//           <span className="text-lg lg:text-4xl mx-1 bg-gradient-to-r from-primary via-primary uppercase to-secondary bg-clip-text text-transparent inline-block">
+//             {t("chooseCard.sophomore")}
+//           </span>
+//         </p>
+//       </div>
+//       <div className="flex justify-center md:justify-between items-center flex-wrap md:flex-row gap-4 pt-6">
+//         {features.map((item) => (
+//           <div
+//             key={item.id}
+//             className="sm:h-60 xl:h-auto w-full flex-initial sm:flex-auto md:flex-1 rounded-md bg-white cursor-pointer flex flex-col items-center justify-center lg:gap-y-5 py-6 px-4 xl:py-14 xl:px-8 hover:shadow-[4px_4px_0px_0px_#b54d2f] duration-500"
+//           >
+//             <div className="choose-icon">
+//               <img
+//                 src={`/images/Schools/${item.id}.svg`}
+//                 alt={item.name}
+//                 className="size-16 md:size-20 xl:size-32 rounded-md"
+//               />
+//             </div>
+//             <div className="min-h-24 text-sm sm:text-sm xl:text-lg font-normal text-center">
+//               <p className="leading-[18.75px] py-1 text-black font-semibold">
+//                 {item.name}
+//               </p>
+//               <p className="leading-[16.41px] font-medium pt-1 text-[#0000007D] mx-10 lg:mx-0">
+//                 {item.description}
+//               </p>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default ChooseCard;
+
