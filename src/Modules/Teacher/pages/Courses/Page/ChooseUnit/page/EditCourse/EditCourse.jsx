baad45a7193b0b8143dff70aece77489b8c46f99
fawzybas <fawzy.basuony@depowebeg.com>
2025-01-21 10:10:58 +0200

    fix Edite courses

--- src/Modules/Teacher/pages/Courses/Page/ChooseUnit/page/EditCourse/EditCourse.jsx
+++ src/Modules/Teacher/pages/Courses/Page/ChooseUnit/page/EditCourse/EditCourse.jsx
@@ -8,6 +8,7 @@ import {
   useGetAllSchoolInformationQuery,
   useGetTeacherCoursesQuery,
 } from "../../../../../../../../Redux/data/getDataApiSlice";
+import { LoadingComponents } from "../../../../../../../../App";
 
 const EditCourse = () => {
   const { t, i18n } = useTranslation();
@@ -16,18 +17,29 @@ const EditCourse = () => {
   const [errorMessage, setErrorMessage] = useState(null);
   const { EditCourseID } = useParams();
 
-  const { data: schoolsData = {}, isLoading, isError } = useGetTeacherCoursesQuery();
-  const [UpdateTeacherCourseContent, { isLoading: courseLoading, isError: courseError }] = useUpdateTeacherCourseContentMutation();
+  const {
+    data: schoolsData = {},
+    isLoading,
+    isError,
+  } = useGetTeacherCoursesQuery();
+  const [UpdateTeacherCourseContent] = useUpdateTeacherCourseContentMutation();
   const { data: ALLschoolsData } = useGetAllSchoolInformationQuery();
 
-  const selectedCourse = schoolsData?.data.filter((id) => id.id === EditCourseID)[0];
+  console.log("ALLschoolsData:", ALLschoolsData?.data);
 
-  // إدارة الحالة للقوائم المنسدلة
-  const [selectedSchoolType, setSelectedSchoolType] = useState(selectedCourse?.school || "");
-  const [selectedGrade, setSelectedGrade] = useState(selectedCourse?.grade || "");
-  const [selectedSubject, setSelectedSubject] = useState(selectedCourse?.subject || "");
+  const selectedCourse = schoolsData?.data?.find(
+    (course) => course.id === EditCourseID
+  );
+  const [selectedSchoolType, setSelectedSchoolType] = useState(
+    selectedCourse?.school || ""
+  );
+  const [selectedGrade, setSelectedGrade] = useState(
+    selectedCourse?.grade || ""
+  );
+  const [selectedSubject, setSelectedSubject] = useState(
+    selectedCourse?.subject || ""
+  );
 
-  
   // React Hook Form
   const {
     register,
@@ -37,7 +49,6 @@ const EditCourse = () => {
     formState: { errors },
   } = useForm();
 
- 
   useEffect(() => {
     if (selectedCourse) {
       setValue("title", selectedCourse.title);
@@ -48,202 +59,224 @@ const EditCourse = () => {
     }
   }, [selectedCourse, setValue]);
 
-  // submit function
+  // Handle form submission
   const handleFormSubmit = async (data) => {
     try {
       const formData = {
         id: EditCourseID,
         title: data.title,
         notes: data.orderNotes,
-        school: selectedSchoolType,  
-        grade: selectedGrade,  
-        subject: selectedSubject,   
+        school: selectedSchoolType,
+        grade: selectedGrade,
+        subject: selectedSubject,
       };
 
-      console.log("ALL DATA : ", formData);
+      console.log("Form Data:", formData);
 
       const response = await UpdateTeacherCourseContent(formData).unwrap();
-      console.log(response);
-      if (response.code == 0) {
-        reset();
+      console.log("API Response:", response);
 
+      if (response.code === 0) {
+        reset();
         navigate(`/teacherPanel/courses/${response.data}`);
-      }
-      if (response.code !== 0) {
+      } else {
         setErrorMessage(response.data);
       }
     } catch (error) {
-      console.error("Error adding course:", error);
+      console.error("Error updating course:", error);
     }
   };
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <LoadingComponents/>;
   }
-  if (isError  ) {
+
+  if (isError) {
     return <div>Error fetching data</div>;
   }
-  if (!schoolsData) {
+
+  if (!selectedCourse) {
     return <div>No course found with the provided ID.</div>;
   }
 
   return (
-    <>
-      <div className="lg:ms-5 h-auto ">
-        <form onSubmit={handleSubmit(handleFormSubmit)}>
-          <div className="w-full lg:w-1/2 sm:mx-auto lg:mx-0">
-            <p className="block text-2xl lg:text-3xl font-semibold">
-              {t("application.addNewCourse")}
-            </p>
-          </div>
+    <div className="lg:ms-5 h-auto">
+      <form onSubmit={handleSubmit(handleFormSubmit)}>
+        <div className="w-full lg:w-1/2 sm:mx-auto lg:mx-0">
+          <p className="block text-2xl lg:text-3xl font-semibold">
+            {t("application.addNewCourse")}
+          </p>
+        </div>
 
-          <div
-            id="options"
-            className="grid grid-cols-1 lg:grid-cols-2 gap-5 h-auto tracking-wide my-4"
-          >
-            {/* Form Section */}
-            <div className="">
-              <div>
-                <label
-                  htmlFor="UserEmail"
-                  className="block text-sm font-medium text-gray-400"
-                >
-                  {t("application.title")}
-                </label>
-                <input
-                  type="text"
-                  id="title"
-                  defaultValue={selectedCourse.title}
-                  {...register("title", {
-                    required: t("application.titleRequired"),
-                  })}
-                  className={`border-2 py-2.5 mt-1 w-full text-gray-600 font-semibold placeholder:font-normal rounded-md shadow-sm sm:text-sm p-2 focus-within:outline-gray-200 bg-[#EFEFEF] ${
-                    errors.title ? "border-red-500" : "border-[#EFEFEF]"
-                  }`}
-                  placeholder={t("application.titlePlaceholder")}
-                />
-                {errors.title && (
-                  <p className="text-red-500 text-sm">{errors.title.message}</p>
-                )}
-              </div>
-
-              <div className="my-4">
-                <label
-                  htmlFor="OrderNotes"
-                  className="block text-sm font-medium text-gray-400"
-                >
-                  {t("application.orderNotes")}
-                </label>
-                <textarea
-                  id="OrderNotes"
-                  defaultValue={selectedCourse.notes}
-                  {...register("orderNotes", {
-                    required: t("application.orderNotesRequired"),
-                  })}
-                  className={`border-2 mt-2 w-full rounded-lg h-[7.5rem] shadow-sm sm:text-sm p-2 text-gray-600 font-semibold placeholder:font-normal focus-within:outline-gray-200 bg-[#EFEFEF] ${
-                    errors.orderNotes ? "border-red-500" : "border-[#EFEFEF]"
-                  }`}
-                  rows="4"
-                  placeholder={t("application.orderNotesPlaceholder")}
-                ></textarea>
-                {errors.orderNotes && (
-                  <p className="text-red-500 text-sm">
-                    {errors.orderNotes.message}
-                  </p>
-                )}
-              </div>
+        <div
+          id="options"
+          className="grid grid-cols-1 lg:grid-cols-2 gap-5 h-auto tracking-wide my-4"
+        >
+          {/* Form Section */}
+          <div className="">
+            <div>
+              <label
+                htmlFor="UserEmail"
+                className="block text-sm font-medium text-gray-400"
+              >
+                {t("application.title")}
+              </label>
+              <input
+                type="text"
+                id="title"
+                defaultValue={selectedCourse.title}
+                {...register("title", {
+                  required: t("application.titleRequired"),
+                })}
+                className={`border-2 py-2.5 mt-1 w-full text-gray-600 font-semibold placeholder:font-normal rounded-md shadow-sm sm:text-sm p-2 focus-within:outline-gray-200 bg-[#EFEFEF] ${
+                  errors.title ? "border-red-500" : "border-[#EFEFEF]"
+                }`}
+                placeholder={t("application.titlePlaceholder")}
+              />
+              {errors.title && (
+                <p className="text-red-500 text-sm">{errors.title.message}</p>
+              )}
             </div>
 
-            {/* School and Grade Selection */}
-            <div className="h-full">
-              {/* School Type Dropdown */}
-              <div>
-                <label
-                  htmlFor="schoolType"
-                  className="text-gray-400 font-semibold text-sm lg:text-md"
-                >
-                  {t("application.selectSchoolType")}
-                </label>
-                <select
-                  id="schoolType"
-                  className="my-2 py-2 w-full rounded-lg text-sm lg:text-md font-medium text-gray-400 border focus:outline-none"
-                  value={selectedSchoolType}
-                  onChange={(e) => setSelectedSchoolType(e.target.value)}
-                >
-                  <option value="">{t("application.selectSchoolType")}</option>
-                  <option value={selectedCourse.school}> {selectedCourse.school}</option>
-                </select>
-              </div>
-
-              {/* Grades Dropdown */}
-              <div>
-                <label
-                  htmlFor="grade"
-                  className="text-gray-400 font-semibold text-sm lg:text-md"
-                >
-                  {t("application.selectGrade")}
-                </label>
-                <select
-                  id="grade"
-                  className="my-2 py-2 w-full rounded-lg text-sm lg:text-md font-medium text-gray-400 border focus:outline-none"
-                  value={selectedGrade}
-                  onChange={(e) => setSelectedGrade(e.target.value)}
-                >
-                  <option value="">{t("application.selectGrade")}</option>
-                  <option value={selectedCourse.grade}>{selectedCourse.grade}</option>
-                </select>
-              </div>
-
-              {/* Subjects Dropdown */}
-              <div>
-                <label
-                  htmlFor="subject"
-                  className="text-gray-400 font-semibold text-sm lg:text-md"
-                >
-                  {t("application.selectSubject")}
-                </label>
-                <select
-                  id="subject"
-                  className="my-2 py-2 w-full rounded-lg text-sm lg:text-md font-medium text-gray-400 border focus:outline-none"
-                  value={selectedSubject}
-                  onChange={(e) => setSelectedSubject(e.target.value)}
-                  {...register("subject", {
-                    required: t("application.fieldRequired"),
-                  })}
-                >
-                  <option value="">{t("application.selectSubject")}</option>
-                  <option value={selectedCourse.subject}>{selectedCourse.subject}</option>
-                </select>
-                {errors.subject && (
-                  <p className="text-red-500 text-sm">
-                    {errors.subject.message}
-                  </p>
-                )}
-              </div>
-            </div>
-
-            <div className=" flex flex-col gap-y-2">
-              {errorMessage && (
-                <p className="text-red-500 text-sm  w-full block">
-                  {errorMessage}
+            <div className="my-4">
+              <label
+                htmlFor="OrderNotes"
+                className="block text-sm font-medium text-gray-400"
+              >
+                {t("application.orderNotes")}
+              </label>
+              <textarea
+                id="OrderNotes"
+                defaultValue={selectedCourse.notes}
+                {...register("orderNotes", {
+                  required: t("application.orderNotesRequired"),
+                })}
+                className={`border-2 mt-2 w-full rounded-lg h-[7.5rem] shadow-sm sm:text-sm p-2 text-gray-600 font-semibold placeholder:font-normal focus-within:outline-gray-200 bg-[#EFEFEF] ${
+                  errors.orderNotes ? "border-red-500" : "border-[#EFEFEF]"
+                }`}
+                rows="4"
+                placeholder={t("application.orderNotesPlaceholder")}
+              ></textarea>
+              {errors.orderNotes && (
+                <p className="text-red-500 text-sm">
+                  {errors.orderNotes.message}
                 </p>
               )}
-
-              {/* Submit Button */}
-              <button
-                type="submit"
-                className="w-full lg:w-1/2 lg:mt-5 rounded bg-primary px-2 py-2 text-md font-semibold text-white hover:bg-blue-800 transition-all duration-300"
-              >
-                {t("application.continue")}
-              </button>
             </div>
           </div>
-        </form>
-      </div>
-    </>
+
+          {/* School and Grade Selection */}
+          <div className="h-full">
+            <div className="mb-4">
+              <label
+                htmlFor="schoolType"
+                className="text-gray-400 font-semibold text-sm lg:text-md block"
+              >
+                {t("application.selectSchoolType")}
+              </label>
+              <select
+                id="schoolType"
+                value={selectedSchoolType}
+                onChange={(e) => {
+                  const schoolId = e.target.value;
+                  setSelectedSchoolType(schoolId);
+                  setSelectedGrade("");
+                  setSelectedSubject("");
+                }}
+                className="w-full py-2 px-3 border rounded-lg text-sm text-gray-500 font-medium focus:outline-none focus:ring focus:ring-blue-200"
+              >
+                <option value="">{t("application.selectSchoolType")}</option>
+                {ALLschoolsData?.data?.map((school) => (
+                  <option key={school.id} value={school.id}>
+                    {i18n.language === "ar" ? school.name_ar : school.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            {/* Dropdown for Grade */}
+            <div className="mb-4">
+              <label
+                htmlFor="grade"
+                className="text-gray-400 font-semibold text-sm lg:text-md block"
+              >
+                {t("application.selectGrade")}
+              </label>
+              <select
+                id="grade"
+                value={selectedGrade}
+                onChange={(e) => {
+                  const gradeId = e.target.value;
+                  setSelectedGrade(gradeId);
+                  // setSelectedSubject("");
+                }}
+                className="w-full py-2 px-3 border rounded-lg text-sm text-gray-500 font-medium  focus:outline-none focus:ring focus:ring-blue-200"
+              >
+                <option value="">{t("application.selectGrade")}</option>
+                {ALLschoolsData?.data
+                  ?.find((school) => school.id === selectedSchoolType)
+                  ?.grades?.map((grade) => (
+                    <option key={grade.id} value={grade.id}>
+                      {i18n.language === "ar" ? grade.name_ar : grade.name}
+                    </option>
+                  ))}
+              </select>
+            </div>
+
+            {/* Dropdown for Subject */}
+            <div className="mb-4">
+              <label
+                htmlFor="subject"
+                className="text-gray-400 font-semibold text-sm lg:text-md block"
+              >
+                {t("application.selectSubject")}
+              </label>
+              <select
+                id="subject"
+                value={selectedSubject}
+                onChange={(e) => {
+                  setSelectedSubject(e.target.value);
+                  setValue("subject", e.target.value);
+                }}
+                className="w-full py-2 px-3 border rounded-lg text-sm text-gray-500 font-medium  focus:outline-none focus:ring focus:ring-blue-200"
+              >
+                <option value="">{t("application.selectSubject")}</option>
+                {ALLschoolsData?.data
+                  ?.find((school) => school.id === selectedSchoolType)
+                  ?.grades?.find((grade) => grade.id === selectedGrade)
+                  ?.subjects?.map((subject) => (
+                    <option key={subject.id} value={subject.id}>
+                      {i18n.language === "ar" ? subject.name_ar : subject.name}
+                    </option>
+                  ))}
+              </select>
+              {errors.subject && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.subject.message}
+                </p>
+              )}
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-y-2">
+            {errorMessage && (
+              <p className="text-red-500 text-sm w-full block">
+                {errorMessage}
+              </p>
+            )}
+
+            {/* Submit Button */}
+            <button
+              type="submit"
+              className="w-full lg:w-1/2 lg:mt-5 rounded bg-primary px-2 py-2 text-md font-semibold text-white hover:bg-blue-800 transition-all duration-300"
+            >
+              {t("application.continue")}
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 };
-
-
 
 export default EditCourse;
