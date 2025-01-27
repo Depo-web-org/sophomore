89a8973d8a59bb4f1b09e3783cfec1a078c7dcbf
osama <152779410+omargad2211@users.noreply.github.com>
2025-01-27 13:29:57 +0200

    feat: implement lesson details

--- src/Modules/Student/pages/CourseVideo/components/CourseInfos/CourseDetails.jsx
+++ src/Modules/Student/pages/CourseVideo/components/CourseInfos/CourseDetails.jsx
@@ -1,17 +1,20 @@
 import { HiInformationCircle } from "react-icons/hi";
+import { useLocation, useParams } from "react-router-dom";
 
 export default function CourseDetails() {
+  const{state}=useLocation()
+  console.log(state)
+  const {lessonID, courseID}= useParams();
+
+  const selectedVideo= state.filter((selected)=> selected.id=== lessonID)[0]
+  console.log(selectedVideo)
+
   return (
     <div className="flex items-start justify-start gap-2">
       
-      <p className="text-xs lg:text-lg xl:text-xl font-semibold text-white">
-      <HiInformationCircle className="text-lg h-8 w-8 text-secondary inline-block " /> Dive into the world of mathematics with this beginner-friendly course
-        designed to build a strong foundation. Explore essential topics like
-        basic arithmetic, fractions, decimals, and an introduction to algebra
-        and geometry. Through engaging lessons and practical examples, you’ll
-        develop problem-solving skills and gain confidence in tackling everyday
-        math challenges. Perfect for learners starting their math journey or
-        looking to strengthen their basics! 4o
+      <p className="text-sm lg:text-xl  font-medium text-gray-300">
+      <HiInformationCircle className="text-lg size-7 text-start text-secondary inline-block " /> 
+      {selectedVideo?.description}
       </p>
     </div>
   );
