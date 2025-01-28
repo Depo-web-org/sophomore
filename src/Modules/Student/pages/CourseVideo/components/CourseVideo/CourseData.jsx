f18b6da7240d5f8a42a75cb60e8416a33b25113e
os1907 <osamatrezey@gmail.com>
2025-01-28 17:18:57 +0200

    style:fix font size

--- src/Modules/Student/pages/CourseVideo/components/CourseVideo/CourseData.jsx
+++ src/Modules/Student/pages/CourseVideo/components/CourseVideo/CourseData.jsx
@@ -8,13 +8,13 @@ import { baseUrl } from "../../../../../../App";
 export default function CourseData() {
   const{state}=useLocation()
   const {lessonID, courseID}= useParams();
-  const selectedVideo= state.filter((selected)=> selected.id=== lessonID)[0]
+  const selectedVideo= state?.filter((selected)=> selected.id=== lessonID)[0]
 
   return (
     <div className=" w-full ">
-      <div className="flex flex-col items-start justify-start gap-2 w-full lg:w-3/4 ">
+      <div className="flex flex-col items-start justify-start gap-2 w-full  ">
         <iframe
-          src={`${baseUrl}${selectedVideo.path}${selectedVideo.video}`}
+          src={`${baseUrl}${selectedVideo?.path}${selectedVideo?.video}`}
           // title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
