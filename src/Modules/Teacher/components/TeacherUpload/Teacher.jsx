01aa6f3c3058c6c12f2fc0751fd7d9a690888d58
fawzybas <fawzy.basuony@depowebeg.com>
2025-01-29 16:52:09 +0200

    size photo change

--- src/Modules/Teacher/components/TeacherUpload/Teacher.jsx
+++ src/Modules/Teacher/components/TeacherUpload/Teacher.jsx
@@ -11,8 +11,9 @@ import { useSelector } from "react-redux";
 import { BsHourglassSplit } from "react-icons/bs";
 import { useAddTeacherDocumentMutation } from "../../../../Redux/data/postDataApiSlice";
 import axios from "axios"; // Import axios for API calls
+import Alert from "../../../Student/pages/Profile/components/Alerts/Alert";
 
-const TeacherUpload = () => {
+const TeacherUpload = (  ) => {
   const role = useSelector((state) => state.role.role);
   const {
     data,
@@ -23,6 +24,7 @@ const TeacherUpload = () => {
   } = useGetProfileTeacherQuery({ provider: role });
   const documentStatus = data?.data?.documents || [];
   const AllDataStatus = data?.data?.status;
+console.log("AllDataStatus :", AllDataStatus);
 
   const { t } = useTranslation();
   const [buttonStates, setButtonStates] = useState([]);
@@ -36,10 +38,14 @@ const TeacherUpload = () => {
     provider: role,
   });
 
+
+  // const [showw, setshoww] = useState();
+  
   // Fetch data from backend on component mount
-  useEffect(() => {
+  useEffect(() => { 
     const fetchData = async () => {
-      setLoading(true); // Set loading to true when data fetching starts
+      setLoading(true);  
+         // Set loading to true when data fetching starts
       try {
         const response = await axios.get(
           "https://dev.depowebeg.com/education/api/getProviderDocumentCategories.php"
@@ -49,6 +55,7 @@ const TeacherUpload = () => {
 
         // Update the cardteacher state with fetched data
         setCardteacher(backendData);
+        //  setshoww(true)
       } catch (error) {
         console.error("Error fetching data from backend:", error);
       } finally {
@@ -114,126 +121,145 @@ const TeacherUpload = () => {
     }
   };
 
+
+ 
+
+
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="relative w-full">
-      <div className="pt-28 lg:pt-32">
-        <TopText
-          name={t("teacherUpload.welcome", {
-            name: `${getUserInformation.data.first_name}`,
-          })}
-          title={t("teacherUpload.uploadPapers")}
+< >
+  
+  {/* {showw && 
+  <Alert  
+            Name={t("changePassword.success")}
+            title={t("changePassword.successMessage")}
+            color={"text-green-600"}
+            showAlert={showAlert}
+            setShowAlert={setShowAlert}
         />
+    }   */}
 
-        <div className="relative inset-x-0 grid grid-cols-2 lg:grid-cols-4 justify-center items-center gap-4 mx-2 sm:mx-10">
-          {loading ? (
-            <p>Loading...</p> // Display loading message or spinner
-          ) : cardteacher.length > 0 ? (
-            cardteacher.map((item, index) => (
-              <div
-                key={item.id}
-                className={`border ${
-                  buttonStates[index]?.status === "Uploaded"
-                    ? "border-green-400"
-                    : "border-white"
-                } h-52 text-center flex flex-col items-center justify-center p-4 rounded-lg`}
-              >
-                <img
-                  className="w-16 h-16 object-cover rounded-md"
-                  src={`https://dev.depowebeg.com/${item.path}/${item.icon}`}
-                  alt={item.name}
-                />
-                <div>
-                  <p className="sm:w-44 min-h-[70px] text-sm sm:text-base lg:text-lg text-white font-bold py-2">
-                    {t(
-                      `teacherUpload.${item.name.toLowerCase().replace(/ /g, "")}`
-                    )}
-                  </p>
-                  <label
-                    className={`${
-                      documentStatus[index]?.verified === "0"
-                        ? "bg-green-500"
-                        : documentStatus[index]?.verified === "2"
-                        ? "bg-orange-500"
-                        : "bg-primary"
-                    } rounded px-4 py-2 text-md font-semibold text-white transition-all duration-300`}
-                  >
-                    {documentStatus[index]?.verified === "1" &&
-                      "upload to verify"}
-                    {documentStatus[index]?.verified === "2" && (
-                      <>
-                        pending verification{" "}
-                        <BsHourglassSplit className="text-white text-xl animate-spin duration-700 transition-all inline-block" />
-                      </>
-                    )}
-                    {documentStatus[index]?.verified === "0" && "verified"}
-                    {documentStatus[index]?.verified !== "1" &&
-                      documentStatus[index]?.verified !== "2" &&
-                      documentStatus[index]?.verified !== "0" &&
-                      (buttonStates[index]?.status === "Uploaded"
-                        ? t("teacherUpload.uploaded")
-                        : t("teacherUpload.upload"))}
-
-                    <input
-                      type="file"
-                      className="hidden"
-                      onChange={(e) => handleFileChange(e, index)}
-                      accept={
-                        item.name === "Intro Video"
-                          ? "video/mp4"
-                          : "application/pdf"
-                      }
-                    />
-                  </label>
+      <form onSubmit={handleSubmit(onSubmit)} className="relative w-full">
+        <div className="pt-28 lg:pt-32">
+          <TopText
+            name={t("teacherUpload.welcome", {
+              name: `${getUserInformation.data.first_name}`,
+            })}
+            title={t("teacherUpload.uploadPapers")}
+          />
+  
+          <div className="relative inset-x-0 grid grid-cols-2 lg:grid-cols-4 justify-center items-center gap-4 mx-2 sm:mx-10">
+            {loading ? (
+              <p>Loading...</p> // Display loading message or spinner
+            ) : cardteacher.length > 0 ? (
+              cardteacher.map((item, index) => (
+                <div
+                  key={item.id}
+                  className={`border ${
+                    buttonStates[index]?.status === "Uploaded"
+                      ? "border-green-400"
+                      : "border-white"
+                  } h-52 text-center flex flex-col items-center justify-center p-4 rounded-lg`}
+                >
+                  <img
+                    className="w-16 h-16 object-cover rounded-md"
+                    src={`https://dev.depowebeg.com/${item.path}/${item.icon}`}
+                    alt={item.name}
+                  />
+                  <div>
+                    <p className="sm:w-44 min-h-[70px] text-sm sm:text-base lg:text-lg text-white font-bold py-2">
+                      {t(
+                        `teacherUpload.${item.name.toLowerCase().replace(/ /g, "")}`
+                      )}
+                    </p>
+                    <label
+                      className={`${
+                        documentStatus[index]?.verified === "0"
+                          ? "bg-green-500"
+                          : documentStatus[index]?.verified === "2"
+                          ? "bg-orange-500"
+                          : "bg-primary"
+                      } rounded px-4 py-2 text-md font-semibold text-white transition-all duration-300`}
+                    >
+                      {documentStatus[index]?.verified === "1" &&
+                        "upload to verify"}
+                      {documentStatus[index]?.verified === "2" && (
+                        <>
+                          pending verification{" "}
+                          <BsHourglassSplit className="text-white text-xl animate-spin duration-700 transition-all inline-block" />
+                        </>
+                      )}
+                      {documentStatus[index]?.verified === "0" && "verified"}
+                      {documentStatus[index]?.verified !== "1" &&
+                        documentStatus[index]?.verified !== "2" &&
+                        documentStatus[index]?.verified !== "0" &&
+                        (buttonStates[index]?.status === "Uploaded"
+                          ? t("teacherUpload.uploaded")
+                          : t("teacherUpload.upload"))}
+  
+                      <input
+                        type="file"
+                        className="hidden"
+                        onChange={(e) => handleFileChange(e, index)}
+                        accept={
+                          item.name === "Intro Video"
+                            ? "video/mp4"
+                            : "application/pdf"
+                        }
+                      />
+                    </label>
+                  </div>
                 </div>
-              </div>
-            ))
+              ))
+            ) : (
+              <p>Loading...</p>
+            )}
+          </div>
+        </div>
+  
+        <div className="relative inset-x-0 text-center py-14">
+          {dataerror ? (
+            <p className="text-red-500 mt-2">
+              {t("teacherUpload.errorUploading")}
+            </p>
           ) : (
-            <p>Loading...</p>
+            <p className="text-lg sm:text-2xl lg:text-3xl text-white font-bold">
+              {t("teacherUpload.waitForApproval")}
+            </p>
+          )}
+  
+          {AllDataStatus !== "0" && (
+            <button
+              type="submit"
+              disabled={loading}
+              className="rounded mt-4 bg-primary px-4 py-2 text-md font-semibold text-white hover:bg-blue-800 transition-all duration-300"
+            >
+              {loading ? (
+                <div
+                  className="inline-block h-5 w-5 animate-spin rounded-full border-2 border-solid border-current border-e-transparent align-[-0.125em] text-white motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                  role="status"
+                >
+                  <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                    {t("teacherUpload.loading")}
+                  </span>
+                </div>
+              ) : (
+                 "Upload Documents"
+   
+              )}
+            </button>
+          )}
+          {AllDataStatus === "0" && (
+            <Link
+              to={"/teacherpanel"}
+              className="rounded mt-5 inline-block bg-primary px-4 py-2 text-md font-semibold text-white hover:bg-blue-800 transition-all duration-300"
+            >
+              Lets start
+            </Link>
           )}
         </div>
-      </div>
-
-      <div className="relative inset-x-0 text-center py-14">
-        {dataerror ? (
-          <p className="text-red-500 mt-2">
-            {t("teacherUpload.errorUploading")}
-          </p>
-        ) : (
-          <p className="text-lg sm:text-2xl lg:text-3xl text-white font-bold">
-            {t("teacherUpload.waitForApproval")}
-          </p>
-        )}
-
-        {AllDataStatus !== "0" && (
-          <button
-            type="submit"
-            disabled={isLoading}
-            className="rounded mt-4 bg-primary px-4 py-2 text-md font-semibold text-white hover:bg-blue-800 transition-all duration-300"
-          >
-            {isLoading ? (
-              <div
-                className="inline-block h-5 w-5 animate-spin rounded-full border-2 border-solid border-current border-e-transparent align-[-0.125em] text-white motion-reduce:animate-[spin_1.5s_linear_infinite]"
-                role="status"
-              >
-                <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-                  {t("teacherUpload.loading")}
-                </span>
-              </div>
-            ) : (
-              "Loading..."
-            )}
-          </button>
-        )}
-        {AllDataStatus == "0" && (
-          <Link
-            to={"/teacherpanel"}
-            className="rounded mt-5 inline-block bg-primary px-4 py-2 text-md font-semibold text-white hover:bg-blue-800 transition-all duration-300"
-          >
-            Lets start
-          </Link>
-        )}
-      </div>
-    </form>
+      </form>
+</ >
   );
 };
 
