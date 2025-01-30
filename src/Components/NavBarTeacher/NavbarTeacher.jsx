1465f64310463407e6e257e0198be0951636b427
os1907 <osamatrezey@gmail.com>
2025-01-30 12:09:14 +0200

    feat:logout teacher

--- src/Components/NavBarTeacher/NavbarTeacher.jsx
+++ src/Components/NavBarTeacher/NavbarTeacher.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "/images/logos/logo.svg";
 import { Link } from "react-router-dom";
 import "../../Components/Navbar/Navbar";
+import { IoExitOutline } from "react-icons/io5";
+import { useTranslation } from "react-i18next";
+import LogoutModal from "../../Modules/Student/pages/Profile/components/Security/LogoutModal";
 
 const NavbarTeacher = () => {
+    const { i18n, t } = useTranslation(); // Get the i18n instance and t function
+    const [LogOutState, setLogOutState] = useState(false);
+  const isRTL = i18n.language === "ar"; // Check if the current language is Arabic
+
   return (
+   <>
     <nav className="z-50 fixed top-4 w-full px-4 md:px-0 ">
-      <div className="flex justify-center items-center bg-white/20 shadow-black/10  backdrop-blur-[5px] border border-white/20 container w-full md:w-custom-md xl:w-custom-xl  transition-all duration-300 h-16 mx-auto shadow-sm rounded-full  ">
+      <div className="flex justify-between  items-center bg-white/20 shadow-black/10  backdrop-blur-[5px] border border-white/20 container w-full md:w-custom-md xl:w-custom-xl  transition-all duration-300 h-16 mx-auto shadow-sm rounded-full  ">
         <Link to={"/"}>
           <div className="pt-2 lg:ml-5 w-full flex">
             <img src={logo} alt="logo" className=" h-12 lg:h-8 w-auto" />
@@ -15,8 +23,23 @@ const NavbarTeacher = () => {
             </p>
           </div>
         </Link>
+         <div
+                  onClick={() => setLogOutState(!LogOutState)}
+                  className={` group p-2   flex items-center gap-2 rounded-lg transition-all duration-500 px-8 lg:px-12 text-white font-semibold text-lg`}
+                >
+                  <button>
+                    <IoExitOutline className={`w-6 h-6 inline ${isRTL ? "rotate-0" : "rotate-180"}`} />
+                    <span className="ms-3">
+                      {t("sidebar.logout")} {/* Translated label */}
+                    </span>
+                  </button>
+                </div>
       </div>
     </nav>
+    <div className="bg-emerald-600">
+        {LogOutState && <LogoutModal setOpseModel={setLogOutState} />}
+      </div>
+   </>
   );
 };
 
